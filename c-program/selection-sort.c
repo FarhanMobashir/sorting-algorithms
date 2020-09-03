6aa273cd90a43d6575fff3758921f38ad34e62e0
Mobashir Farhan <farhanmobashir007@gmail.com>
2020-09-04 00:25:12 +0530

    adds selection sort program in c

--- c-program/selection-sort.c
+++ c-program/selection-sort.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
 
+int main(void)
+
+{
+    int i,j,count,temp,number[25];
+
+    printf("How many number you want to sort: ");
+    scanf("%d", &count);
+
+    printf("Enter %d elements: ", count);
+
+    // Loop to get elemnts in unsorted array
+
+    for(int i = 0; i < count; i++) {
+        scanf("%d", &number[i]);
+    }
+
+    // logic for selection  sort
+
+    for(i = 0; i < count; i++) {
+        for(int j =i +1; j < count; j++) {
+            if(number[j] < number[i]) {
+                temp = number[i];
+                number[i] = number[j];
+                number[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array: ");
+    for(i = 0; i < count; i++) {
+        printf("%d", number[i]);
+    }
+    return 0;
+}
